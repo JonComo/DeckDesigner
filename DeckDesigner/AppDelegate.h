bbabd94ce1850f3e74a6ce1f72236463dc4bfb20
joncomo <jon@underground.net>
2015-08-15 16:30:50 -0700

    Initial Commit

---
+++ DeckDesigner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckDesigner
+//
+//  Created by Jon Como on 8/15/15.
+//  Copyright (c) 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckDesigner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckDesigner
+//
+//  Created by Jon Como on 8/15/15.
+//  Copyright (c) 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
